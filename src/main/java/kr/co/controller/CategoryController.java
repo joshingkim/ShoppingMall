1a7258be031b3bbdb3eaa42ca5586e061ccd1a40
jongsik kim <rlawhdtlr8@gmail.com>
2022-01-04 16:04:07 +0900

    카테고리 수정 수정 v1.1

--- src/main/java/kr/co/controller/CategoryController.java
+++ src/main/java/kr/co/controller/CategoryController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.domain.CategoryVO;
-import kr.co.domain.MemberVO;
 import kr.co.domain.PageTO;
 import kr.co.service.CategoryService;
 
