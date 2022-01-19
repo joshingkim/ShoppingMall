888a22fc1c377a43697cd8dac4b9578625ac6fd4
kjh56489 <kjh56489@gmail.com>
2022-01-19 12:27:48 +0900

    배송완료

--- src/main/java/kr/co/repository/OrderDAO.java
+++ src/main/java/kr/co/repository/OrderDAO.java
@@ -1,12 +1,18 @@
 package kr.co.repository;
 
+import java.util.List;
+
 import kr.co.domain.OrderDetailVO;
 import kr.co.domain.OrderVO;
+import kr.co.domain.OrdersVO;
 
 public interface OrderDAO {
 
 	public void insert(OrderVO vo);
 
 	public OrderVO detail(int order_item_no);
+
+	public void insert(OrdersVO ovo);
+
 
 }
--- src/main/java/kr/co/repository/OrderDAOImpl.java
+++ src/main/java/kr/co/repository/OrderDAOImpl.java
@@ -1,11 +1,14 @@
 package kr.co.repository;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import kr.co.domain.OrderVO;
+import kr.co.domain.OrdersVO;
 
 @Repository
 public class OrderDAOImpl implements OrderDAO{
@@ -17,13 +20,19 @@ public class OrderDAOImpl implements OrderDAO{
 
 	@Override
 	public void insert(OrderVO vo) {
-		sqlSession.insert(NS+".insert", vo);
+		sqlSession.insert(NS+".insert1", vo);
 	}
 
 	@Override
 	public OrderVO detail(int order_item_no) {
 		return sqlSession.selectOne(NS+".detail", order_item_no);
 	}
+
+	@Override
+	public void insert(OrdersVO ovo) {
+		sqlSession.insert(NS+".insert", ovo);
+	}
+
 	
 	
 	
