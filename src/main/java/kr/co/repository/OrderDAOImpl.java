120f5fdcd99709be63bbf278c7e91e20a24a7852
kjh56489 <kjh56489@gmail.com>
2022-01-20 18:07:17 +0900

    주문및배송조회완료

--- src/main/java/kr/co/repository/OrderDAOImpl.java
+++ src/main/java/kr/co/repository/OrderDAOImpl.java
@@ -1,13 +1,14 @@
 package kr.co.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
-import kr.co.domain.OrderVO;
 import kr.co.domain.OrdersVO;
 
 @Repository
@@ -18,15 +19,6 @@ public class OrderDAOImpl implements OrderDAO{
 	
 	private final String NS = "kr.co.order";
 
-	@Override
-	public void insert(OrderVO vo) {
-		sqlSession.insert(NS+".insert1", vo);
-	}
-
-	@Override
-	public OrderVO detail(int order_item_no) {
-		return sqlSession.selectOne(NS+".detail", order_item_no);
-	}
 
 	@Override
 	public void insert(OrdersVO ovo) {
@@ -37,6 +29,36 @@ public class OrderDAOImpl implements OrderDAO{
 	public List<OrdersVO> list(String member_id) {
 		return sqlSession.selectList(NS+".list", member_id);
 	}
+
+	@Override
+	public List<OrdersVO> list_manager(List<OrdersVO> vo) {
+		return sqlSession.selectList(NS+".list_manager", vo);
+	}
+
+	@Override
+	public void status(OrdersVO vo) {
+		sqlSession.update(NS+".status", vo);
+	}
+
+	@Override
+	public List<OrdersVO> mlist(OrdersVO vo, int count) {
+		RowBounds rb = new RowBounds(0, count);
+		return sqlSession.selectList(NS+".mlist2", vo, rb);
+	}
+
+	@Override
+	public String getOrder_date(int order_id) {
+		return sqlSession.selectOne(NS+".getOrder_date", order_id);
+	}
+
+	@Override
+	public OrdersVO checkorder(int order_id) {
+		return sqlSession.selectOne(NS + ".checkorder", order_id);
+	}
+
+	
+
+
 
 	
 
