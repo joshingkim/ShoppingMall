e1ebd34afada9ad58ba2bfb7152844755d7f69d8
Elliot <uranoselpis@naver.com>
2022-01-24 03:48:40 +0900

    날짜수정완료

--- src/main/java/kr/co/domain/LikeItemVO.java
+++ src/main/java/kr/co/domain/LikeItemVO.java
@@ -1,7 +1,6 @@
 package kr.co.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Objects;
 
 public class LikeItemVO implements Serializable{
@@ -10,7 +9,7 @@ public class LikeItemVO implements Serializable{
 	private int like_no;
 	private int item_no;
 	private String member_id;
-	private Date like_date;
+	private String like_date;
 	private String item_name;
 	private String file_name;
 	
@@ -22,7 +21,7 @@ public class LikeItemVO implements Serializable{
 		this.member_id = member_id;
 	}
 
-	public LikeItemVO(int like_no, int item_no, String member_id, Date like_date) {
+	public LikeItemVO(int like_no, int item_no, String member_id, String like_date) {
 		this.like_no = like_no;
 		this.item_no = item_no;
 		this.member_id = member_id;
@@ -31,7 +30,7 @@ public class LikeItemVO implements Serializable{
 	
 	
 
-	public LikeItemVO(int like_no, int item_no, String member_id, Date like_date, String item_name, String file_name) {
+	public LikeItemVO(int like_no, int item_no, String member_id, String like_date, String item_name, String file_name) {
 		super();
 		this.like_no = like_no;
 		this.item_no = item_no;
@@ -81,11 +80,11 @@ public class LikeItemVO implements Serializable{
 		this.member_id = member_id;
 	}
 
-	public Date getLike_date() {
+	public String getLike_date() {
 		return like_date;
 	}
 
-	public void setLike_date(Date like_date) {
+	public void setLike_date(String like_date) {
 		this.like_date = like_date;
 	}
 
